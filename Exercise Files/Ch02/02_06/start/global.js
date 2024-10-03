adf117e8fd4a5be38779e08f9eae7b1d451604bc
Fortune-Ndlovu <fndlovu@redhat.com>
2024-10-03 22:40:07 +0100

    Understanding core modules

--- Exercise Files/Ch02/02_06/start/global.js
+++ Exercise Files/Ch02/02_06/start/global.js
@@ -1,5 +1,11 @@
-const waitTime = 3000;
-console.log(`setting a ${waitTime / 1000} second delay`);
-const timerFinished = () => console.log("done");
+let currentTime = 0;
+const incTime = () => {
+    currentTime += waitInterval;
+    const p = Math.floor((currentTime/waitTime) * 100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`waiting.... ${p}`);
+    console.log(`waiting ${currentTime / 1000} seconds`);
+};
 
-setTimeout(timerFinished, waitTime);
+const interval = setInterval(incTime, waitInterval);
