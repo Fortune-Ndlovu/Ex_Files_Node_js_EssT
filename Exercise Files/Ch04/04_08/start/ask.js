caa5b5a9695f87a99e4d72350166dd1c155572b6
Fortune Ndlovu <fndlovu@redhat.com>
2024-10-26 23:56:36 +0100

    Update ask.js

--- Exercise Files/Ch04/04_08/start/ask.js
+++ Exercise Files/Ch04/04_08/start/ask.js
@@ -18,21 +18,23 @@ function ask(i) {
 process.stdin.once("data", (data) => {
   let name = data.toString().trim();
   let fileName = `./${name}.md`;
-
   if (fs.existsSync(fileName)) {
     fs.unlinkSync(fileName);
   }
-
   answerStream = fs.createWriteStream(fileName);
-  answerStream.write(`Question Answers for ${name}\n===========\n`);
+  answerStream.write(
+    `Question Answers for ${name}\n========\n`
+  );
 });
 
-process.stdin.on("data", (data) => {
+process.stdin.on("data", function (data) {
   let answer = data.toString().trim();
 
-  answerStream.write(`Question: ${questions[answers.length]}\n`);
+  answerStream.write(
+    `Question: ${questions[answers.length]}\n`
+  );
 
-  answerStream.write(`Answer: ${answer}\n`, function() {
+  answerStream.write(`Answer: ${answer}\n`, function () {
     if (answers.length < questions.length) {
       ask(answers.length);
     } else {
