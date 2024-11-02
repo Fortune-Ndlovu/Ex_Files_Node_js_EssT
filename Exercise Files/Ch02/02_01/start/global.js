21494743051b688bf4376efe3812ade14992ea70
Fortune Ndlovu <fndlovu@redhat.com>
2024-11-02 23:55:30 +0000

    Update global.js

--- Exercise Files/Ch02/02_01/start/global.js
+++ Exercise Files/Ch02/02_01/start/global.js
@@ -14,3 +14,6 @@ const server = createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// We set the Content-Type header
+res.setHeader('Content-Type', 'text/plain');
