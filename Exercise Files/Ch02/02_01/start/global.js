ce0867a58fcd58319f9cb7c213b7582040c7626f
Fortune Ndlovu <fndlovu@redhat.com>
2024-11-02 23:54:25 +0000

    Update global.js

--- Exercise Files/Ch02/02_01/start/global.js
+++ Exercise Files/Ch02/02_01/start/global.js
@@ -1,3 +1,16 @@
 let hello = "Hello world from Node.js";
 let justNode = hello.slice(17);
 console.log(`Who let the ${justNode} out?`);
+
+////
+const { createServer } = require('node:http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
